644c12edfece0dc74ab95e6794226a71696e9ea2
Timur Martinez-Mukimov <sonictimur@gmail.com>
2024-01-14 01:59:13 +0500

    tested builds to the web, still needs work

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,21 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
     commands.spawn(Camera2dBundle::default());
     commands.spawn(SpriteBundle {
         texture: asset_server.load("ducky.png"),
