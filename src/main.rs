4f3d566329b89595388f619302a013e3f6a7368a
Timur Martinez-Mukimov <sonictimur@gmail.com>
2024-01-14 00:50:26 +0500

    les go

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("ducky.png"),
+        ..Default::default()
+    });
+}
